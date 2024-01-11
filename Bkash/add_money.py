d3d93c2481ff55dfd2b52cf7396f05861c09cb6a
Fida Zaman <88763525+FidaZaman@users.noreply.github.com>
2024-01-12 01:37:43 +0600

    imported to main

---
+++ Bkash/add_money.py
@@ -0,0 +1,6 @@
+def add_money():
+  print("====== Hi! i am jini. =======\n I can add any amount of money to your account \n")
+  a_amount = int(input("how many money you want? "))
+  pin = int(input("enter pin :"))
+  if a_amount > 0:
+    print("""pin dilei ki ar na dilei ki \n Taka gache dhore? eijonnoi apni banlagi. 😑 egula na kore ektu porte boshen oita beshi kaje dibe.😅 """)
